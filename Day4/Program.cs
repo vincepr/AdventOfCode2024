ca3790da5d24a735d73b418b16e63f23f2db7f0a
vincepr <vincepr@gmail.com>
2024-12-25 14:24:25 +0100

    Day5FirstPart

---
+++ Day4/Program.cs
@@ -0,0 +1,79 @@
+﻿List<List<char>> test = [
+    ['A', 'B', 'C', 'D'],
+    ['1', '2', '3', '4'],
+    ['E', 'F', 'G', 'H'],
+    ['5', '6', '7', '8']
+];
+var x = GetDiagonals(test);
+foreach(var z in x)
+    Console.WriteLine(z);
+
+Console.WriteLine("starting");
+var lines = File.ReadAllLines("./input4.csv").ToList();
+List<string> wordsToSearch = ["XMAS"];
+List<int> contains = [];
+
+foreach (var word in wordsToSearch)
+{
+    // search left to right
+    foreach (var line in lines)
+    {
+        contains.AddRange(ContainsIndexes(line, word));
+    }
+    // search right to left
+    foreach (var line in lines)
+    {
+        contains.AddRange(ContainsIndexes(Reverse(line), word));
+    }
+}
+// build diagonals
+// 0 // 0 1 2 3 4 5 6 7 8 9
+// 1 // 0 1 2 3 4 5 6 7 8 9
+// 2 // 0 1 2 3 4 5 6 7 8 9
+// 3 // 0 1 2 3 4 5 6 7 8 9
+// 4 // 0 1 2 3 4 5 6 7 8 9
+// 5 // 0 1 2 3 4 5 6 7 8 9
+// 6 // 0 1 2 3 4 5 6 7 8 9
+// 7 // 0 1 2 3 4 5 6 7 8 9
+// 8 // 0 1 2 3 4 5 6 7 8 9
+// 9 // 0 1 2 3 4 5 6 7 8 9
+// (0,0m)
+// (0,1) (1,0)
+// (0,2) (1,1) (2,0)
+// (0,3) (1,2) (2,1) (3,0)
+
+System.Console.WriteLine(contains.Count);
+
+IEnumerable<int> ContainsIndexes(string line, string word)
+{
+    return System.Text.RegularExpressions.Regex.Matches(line, word)
+        .Cast<System.Text.RegularExpressions.Match>()
+        .Select(m => m.Index);
+}
+static string Reverse(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+static IEnumerable<string> GetDiagonals(List<List<char>> lines)
+{
+    for (var len = 0; len < lines.Count; len++)
+    {
+        string str = "";
+        for (var i = 0; i <= len; i++)
+        {
+            str += lines[len - i][i - len + len];
+        }
+        if (str != string.Empty)
+            yield return str;
+
+        for (var i = 0; i <+ len; i++)
+        {
+            str += lines[i][len - i];
+        }
+        if (str != string.Empty)
+            yield return str;
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllLines("./input");
+List<(int, int)> rules = [];
+List<List<int>> lists = [];
+foreach (var str in input)
+{
+    if (str == "")
+    {
+        continue;
+    }
+    if (str.Contains("|"))
+    {
+        var split = str.Split("|");
+        rules.Add((int.Parse(split[0]), int.Parse(split[1])));
+    }
+    if (str.Contains(","))
+    {
+        var nrs = str.Split(",").Select(nr => int.Parse(nr)).ToList();
+        lists.Add(nrs);
+    }
+}
+
+List<int> successMiddles = [];
+foreach (var list in lists)
+{
+    var isTrue = true;
+    foreach (var rule in rules)
+    {
+        var idx1 = list.FindIndex(itm => itm == rule.Item1);
+        var idx2 = list.FindIndex(itm => itm == rule.Item2);
+        if (idx1 != -1 && idx2 != -1)
+        {
+            if (idx1 > idx2){
+                isTrue = false;
+            }
+        }
+    }
+    if (isTrue)
+    {
+        System.Console.WriteLine(string.Join(",", list));
+        System.Console.WriteLine($"Added {list[list.Count / 2]}");
+        successMiddles.Add(list[list.Count / 2]);
+    }
+}
+System.Console.WriteLine($"SumMiddles: {successMiddles.Sum()}");
